d41ffe48532400ebed1689c796d659c9c43ccec8
Chinatsu Kato <cnt.tkhs@gmail.com>
2020-05-06 18:19:54 +0900

    Set up environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Typescript Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-typescript`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
