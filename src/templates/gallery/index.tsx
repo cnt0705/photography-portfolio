483abacf4a51b651332368f3cd3003d0a691462d
Chinatsu Kato <cnt.tkhs@gmail.com>
2020-05-31 17:08:40 +0900

    Put styles for desktop on gallery page

--- src/templates/gallery/index.tsx
+++ src/templates/gallery/index.tsx
@@ -9,6 +9,7 @@ import { GalleryQuery } from '../../../types/graphql-types' // TODO: Path
 import { useGallery } from './hooks/useGallery'
 
 import { Layout } from 'layouts'
+import { mq } from 'styles/media-queries'
 import SiteName from './assets/sc.svg'
 
 type PageContext = { photographer: string; instagram: string }
@@ -23,32 +24,80 @@ const Template: React.FC<Props> = ({ data, pageContext }) => {
 
   return (
     <Layout>
-      <nav css={nav}>
-        <Link to="/" css={siteTitle}>
-          <SiteName />
-        </Link>
-        <a
-          href={`https://www.instagram.com/${pageContext.instagram}`}
-          target="_blank"
-          rel="noopener"
-          css={photographer}
-        >
-          #{pageContext.photographer}Kato
-        </a>
-      </nav>
-      <div css={gallery}>
-        <Gallery photos={photos} />
+      <div css={container}>
+        <div css={containerInner}>
+          <nav css={nav}>
+            <Link to="/" css={siteTitle}>
+              <SiteName />
+            </Link>
+            <a
+              href={`https://www.instagram.com/${pageContext.instagram}`}
+              target="_blank"
+              rel="noopener"
+              css={photographer}
+            >
+              #{pageContext.photographer}Kato
+            </a>
+          </nav>
+          <div css={gallery}>
+            <Gallery photos={photos} />
+          </div>
+        </div>
       </div>
     </Layout>
   )
 }
+
+const container = css`
+  background-color: #222;
+
+  ${mq.desktop} {
+    position: relative;
+    z-index: 0;
+
+    &::after {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      width: 100%;
+      height: 330px;
+      background-color: #fff;
+      content: '';
+    }
+  }
+`
+
+const containerInner = css`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  ${mq.desktop} {
+    flex-direction: row;
+    align-items: flex-start;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 140px 0;
+  }
+`
 
 const nav = css`
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 50px 30px;
+  background-color: #fff;
   color: #888;
+
+  ${mq.desktop} {
+    flex-direction: column;
+    align-items: flex-start;
+    width: 280px;
+    padding: 0 20px;
+    background-color: inherit;
+  }
 `
 
 const siteTitle = css`
@@ -59,11 +108,19 @@ const photographer = css`
   line-height: 1;
   font-size: 1.8rem;
   letter-spacing: 2px;
+
+  ${mq.desktop} {
+    margin-top: 50px;
+  }
 `
 
 const gallery = css`
+  flex: 1;
   padding: 30px;
-  background: #222;
+
+  ${mq.desktop} {
+    padding: 0;
+  }
 `
 
 export const pageQuery = graphql`
