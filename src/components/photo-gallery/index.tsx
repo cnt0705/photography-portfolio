ea95c2abf0d653193c2eb861d89532ee65bfd454
Chinatsu Kato <cnt.tkhs@gmail.com>
2020-06-06 21:51:00 +0900

    Add react-images

--- src/components/photo-gallery/index.tsx
+++ src/components/photo-gallery/index.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import Gallery, { PhotoProps } from 'react-photo-gallery'
+import Carousel, { Modal, ModalGateway } from 'react-images'
 
 type Props = {
   photos: PhotoProps[]
 }
 
 export const PhotoGallery: React.FC<Props> = ({ photos }) => {
-  return <Gallery photos={photos} />
+  const [currentImage, setCurrentImage] = useState(0)
+  const [viewerIsOpen, setViewerIsOpen] = useState(false)
+
+  const openLightbox = useCallback((_event, { index }) => {
+    setCurrentImage(index)
+    setViewerIsOpen(true)
+  }, [])
+
+  const closeLightbox = () => {
+    setCurrentImage(0)
+    setViewerIsOpen(false)
+  }
+
+  return (
+    <>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(p => ({
+                source: p.src,
+                caption: p.alt,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </>
+  )
 }
