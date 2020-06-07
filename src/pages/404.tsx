14e7326b1eacfaedf7504640790c07ab214fd99e
Chinatsu Kato <cnt.tkhs@gmail.com>
2020-06-07 15:54:39 +0900

    Install react-rough-notation

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'gatsby-link'
 import { css } from '@emotion/core'
 
+import { RoughNotation } from 'react-rough-notation'
+
 import NotFound from 'assets/not-found.svg'
 import { mq } from 'styles/media-queries'
 
@@ -10,7 +12,15 @@ const NotFoundPage = () => (
     <NotFound />
     <p css={pageNotFound}>Page Not Found</p>
     <p css={goBackHome}>
-      <Link to="/">Go Back Home</Link>
+      <RoughNotation
+        animationDelay={750}
+        padding={20}
+        show={true}
+        strokeWidth={1}
+        type="box"
+      >
+        <Link to="/">Go Back Home</Link>
+      </RoughNotation>
     </p>
   </div>
 )
@@ -35,10 +45,11 @@ const pageNotFound = css`
 `
 
 const goBackHome = css`
-  margin-top: 50px;
+  margin-top: 70px;
   font-size: 2.4rem;
 
   ${mq.desktop} {
+    margin-top: 100px;
     font-size: 2.8rem;
   }
 `
