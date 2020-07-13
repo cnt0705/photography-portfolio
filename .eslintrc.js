955c32e8e038d496bf7337a2e0316778c7c36800
Chinatsu Kato <cnt.tkhs@gmail.com>
2020-07-13 17:46:10 +0900

    chore: Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,11 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:prettier/recommended',
-    'prettier/react',
     'prettier/@typescript-eslint',
+    'prettier/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
